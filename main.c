b4d214772c6344d3159b47ae53e29eb1005c3909
zgaoq <1160143477@qq.com>
2016-09-16 20:57:48 +0800

    增加commonsub功能接口
    增加commonsub功能接口，查找str1与str2的最大公共子串

--- main.c
+++ main.c
@@ -1,4 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_STRING_KEN 100
+
+
+void commonSub(char *sub)
+{
+  int i = 0, j = 0, k = 0, maxpos = -1, maxlen = 0;
+  char str1[MAX_STRING_KEN] = "asdfghjkl";
+  char str2[MAX_STRING_KEN] = "sdjkl";
+
+  for (i = 0; i < strlen(str1); i++)
+  {
+    for (j = 0; j < strlen(str2); j++)
+    {
+      
+    }
+  }
+}
+
 main
 {
-  printf("for test");
+    printf("for test");
+    char sub[MAX_STRING_KEN] = {0};
+
+    commonSub(sub);
+    
+    printf("\n sub string is : %s", sub);
 }
